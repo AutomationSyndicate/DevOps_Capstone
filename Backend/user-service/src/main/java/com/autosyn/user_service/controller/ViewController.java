7ed6a0aa6b27b575f8ea2dccde6a832cb0999c22
MasbiMpho <mpholin15@gmail.com>
2025-04-25 15:48:52 +0200

    Added admin and user dashboard frontend but backend does not work

--- Backend/user-service/src/main/java/com/autosyn/user_service/controller/ViewController.java
+++ Backend/user-service/src/main/java/com/autosyn/user_service/controller/ViewController.java
@@ -1,5 +1,6 @@
 package com.autosyn.user_service.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -22,7 +23,26 @@ public class ViewController {
     }
 
     @GetMapping("/dashboard")
-    public String dashboardPage() {
-        return "dashboard";
+    public String dashboardPage(Authentication auth) {
+
+        if (auth.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"))) {
+            return "redirect:/dashboard/admin";
+        }
+        else if (auth.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_USER"))) {
+            return "redirect:/dashboard/user";
+        }
+
+
+        return "error";
+    }
+
+    @GetMapping("/dashboard/admin")
+    public String admindashboardPage() {
+        return "admin-dashboard";
+    }
+
+    @GetMapping("/dashboard/user")
+    public String userdashboardPage() {
+        return "user-dashboard";
     }
 }
